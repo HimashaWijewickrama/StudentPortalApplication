33a62e22db44f619baa1effed017a0ff207250d3
myusername <himashawije5678@gmail.com>
2025-12-27 01:17:49 +0530

    ✨this view allows to delete a student

--- obj/Debug/net8.0/StudentPortalApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentPortalApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bf42dab054052bafa1903a73a5deb1709b453fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32adcd15b036b4a5be5a52f212e13bf59a56614b")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
