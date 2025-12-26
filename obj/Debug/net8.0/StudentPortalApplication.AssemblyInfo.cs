8bf42dab054052bafa1903a73a5deb1709b453fe
myusername <himashawije5678@gmail.com>
2025-12-27 01:05:12 +0530

    ✨this view shows detailed information about a selected student

--- obj/Debug/net8.0/StudentPortalApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentPortalApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df8f0e059b4de6816db07cc5e2b4a297eacc4ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4205e819437689a29d490409e4e9587045695687")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
