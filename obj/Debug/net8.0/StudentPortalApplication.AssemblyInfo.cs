fd53049920ad469fe57eb7e7cd3d28bc427e3bb5
myusername <himashawije5678@gmail.com>
2026-02-08 11:03:44 +0530

    feat: add login view with form validations

--- obj/Debug/net8.0/StudentPortalApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentPortalApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543711050fd69cf9a03c2233f935a5e92beb03be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c17694a6b9691f9eadf2a4fe32986aa681ab073")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortalApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
