a83f6f8199d273a222beb71da0ab3968350d29a9
Himasha Wijewickrama <himashawije5678@gmail.com>
2026-02-08 11:43:24 +0530

    Add user authentication rules and user routings (#2)
    * feat: add Identity package for user authentication process
    
    * refactor: update DbContext to inherit from IdentityDbContext
    
    * feat: configure identity services and authentication middleware
    
    * feat: add register view model
    
    * feat: add login view model
    
    * feat: create AccountController with login, register, and logout actions
    
    * feat: add login view with form validations
    
    * chore: add login UI to navigation and footer

---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortalApplication.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength =6, ErrorMessage ="Password must be at least 6 characters long")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and Confirmation password do not match")]
+        public string ConfirmPassword { get; set; } = null!;
+
+        [Required(ErrorMessage = "Full Name is required")]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; } = null!;
+       
+    }
+}
